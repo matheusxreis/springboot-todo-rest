2c6ee76cfb8b27cc06c051a7fc72d95f841c8c91
Matheus Reis <matheus.reis081@gmail.com>
2023-04-04 15:00:26 -0300

    feat: creating routes to list by id, save and update

--- src/main/java/com/matheusxreis/todo/controllers/TaskController.java
+++ src/main/java/com/matheusxreis/todo/controllers/TaskController.java
@@ -1,15 +1,17 @@
 package com.matheusxreis.todo.controllers;
 
 
+import com.matheusxreis.todo.dtos.SaveTaskDTO;
 import com.matheusxreis.todo.models.Task;
 import com.matheusxreis.todo.repositories.TaskRepository;
+import org.hibernate.mapping.Any;
+import org.hibernate.validator.internal.util.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value="/tasks")
@@ -21,6 +23,35 @@ public class TaskController {
     @GetMapping()
     public List<Task> listAll(){
         return repo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Task> getTask(@PathVariable(value="id") long id){
+        return repo.findById(id);
+    }
+
+    @PostMapping()
+    public void saveTask(
+            @RequestBody
+            SaveTaskDTO data
+            ){
+
+        Task task = new Task(
+                data.description,
+                data.owner
+        );
+        repo.save(task);
+    }
+
+    @PatchMapping("{id}")
+    public void mark(
+            @PathVariable(value="id") long id
+    ){
+       Optional<Task> task = repo.findById(id);
+       task.ifPresent(value -> {
+           value.mark();
+           repo.save(value);
+       });
     }
 
 
