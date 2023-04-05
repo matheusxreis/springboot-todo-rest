2a7d304e43249882e4c59b4f765bf410feac4232
Matheus Reis <matheus.reis081@gmail.com>
2023-04-05 11:34:16 -0300

    feat: encoding password

--- src/main/java/com/matheusxreis/todo/services/UserService.java
+++ src/main/java/com/matheusxreis/todo/services/UserService.java
@@ -4,24 +4,28 @@ import com.matheusxreis.todo.models.User;
 import com.matheusxreis.todo.repositories.Repo;
 import com.matheusxreis.todo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.net.PasswordAuthentication;
 
 @Service
 public class UserService {
 
     Repo repo;
+    PasswordEncoder encoder;
     @Autowired
-    UserService(Repo repo){
+    UserService(Repo repo, PasswordEncoder encoder){
        this.repo = repo;
+       this.encoder = encoder;
     }
     public void login(){
 
     }
 
     public void register(String username, String password){
-        User user = new User(username, password);
+
+        User user = new User(username,encoder.encode(password));
         this.repo.user.save(user);
     }
 
