f93ef6b1138b57a4ea39670df7ee7366b739a236
Matheus Reis <matheus.reis081@gmail.com>
2023-04-05 08:49:05 -0300

    fix: using repo instead of user repository in user service

--- src/main/java/com/matheusxreis/todo/services/UserService.java
+++ src/main/java/com/matheusxreis/todo/services/UserService.java
@@ -1,15 +1,19 @@
 package com.matheusxreis.todo.services;
 
+import com.matheusxreis.todo.models.User;
+import com.matheusxreis.todo.repositories.Repo;
 import com.matheusxreis.todo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.PasswordAuthentication;
+
 @Service
 public class UserService {
 
-    UserRepository repo;
+    Repo repo;
     @Autowired
-    UserService(UserRepository repo){
+    UserService(Repo repo){
        this.repo = repo;
     }
     public void login(){
@@ -17,6 +21,7 @@ public class UserService {
     }
 
     public void register(String username, String password){
-
+        User user = new User(username, password);
+        this.repo.user.save(user);
     }
 }
